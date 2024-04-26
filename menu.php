c9c55bb805bfdfe79f1feca3ae6088190e1a48e1
vitor grey <vitormattos2011@gmail.com>
2024-04-25 22:30:02 -0300

    v0.3

--- menu.php
+++ menu.php
@@ -1,11 +1,8 @@
 <?php
-# ============ Menus ============ #
+    # ============ Menus ============ #
 
-$saudação = "Olá, bem-vindo à Barbearia Dois Irmão! Aqui é onde os cabelos recebem o tratamento VIP que merecem!  Vamos fazer você se sentir incrível!, mas primeiro me com quer ser chamado?";
+    $saudação = "Olá, bem-vindo à Barbearia Dois Irmão! Aqui é onde os cabelos recebem o tratamento VIP que merecem!  Vamos fazer você se sentir incrível!, mas primeiro me com quer ser chamado?";
 
-$menu1 = "Escolha uma opção:\n
-1. Entrar na fila\n
-2. Ver fila de espera\n
-3. Ver tabela de preco";
+    $menu1 = "Escolha uma opção:\n1️⃣ - Entra na fila \n2️⃣ - Ver tempo de espera \n3️⃣ - Ver tabela de preços";
 
 ?>
